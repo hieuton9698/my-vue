83421db083363596b74597f7194e6397636fdd9b
hieu <hieuton96@gmail.com>
2025-04-07 10:23:54 +0700

    my-vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { computed, ref } from 'vue';
+const firstName = ref('nguyen')
+const lastName = ref('a')
+const fullName = computed({
+  get(){
+    return firstName.value +' '+ lastName.value
+  },
+  set(newvalue){
+    [firstName.value, lastName.value] = newvalue.split(',')
+  }
+})
+const changeFullname = () => {
+  fullName.value = "nguyen,B"
+}
+</script>
+
+<template>
+ <div>
+  <p id= "count">fullname: {{ fullName }}</p>
+  <p id= "count">first name: {{ firstName }}</p>
+  <p id= "count">last name: {{ lastName }}</p>
+  <button @click="changeFullname">change fullname</button>
+ </div>
+</template>
+
